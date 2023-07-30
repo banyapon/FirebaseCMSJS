7851793897d9b246bbb35af079340fe49c3c6ce6
Banyapon Poolsawas <daydevthailand@gmail.com>
2023-07-30 10:43:06 +0700

    Update Javascript CMS

---
+++ js/init.js
@@ -0,0 +1,11 @@
+var config = {
+  apiKey: "AIzaSyDm3J8DlGu1SqhiXPLkXgmoLpYkYLN1vSM",
+  authDomain: "gangbanggame.firebaseapp.com",
+  projectId: "gangbanggame",
+  storageBucket: "gangbanggame.appspot.com",
+  messagingSenderId: "216418859580",
+  appId: "1:216418859580:web:dfb1368bbbc3fced8f2139",
+  databaseURL: "https://gangbanggame-default-rtdb.firebaseio.com",
+  measurementId: "G-FXNXBGJX2H"
+};
+firebase.initializeApp(config);
